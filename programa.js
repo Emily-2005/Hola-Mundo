594c40600c798b3c29ac464b7104ebab2e46d9f2
Emily-2005 <emily2005rodriguez@gmail.com>
2024-10-31 10:20:45 -0400

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,31 @@
+/**
+ * funcion del programa: decirle al programa que cuente del 1 al 11, pero solamente cuente los numeros impares
+ */
+
+
+let i = 0;
+
+    while (i < 11) {
+
+        i++;
+
+        if (i % 2 == 0) {
+
+            continue;
+            
+        } else {
+
+            console.log( "numero impar", i)
+            
+        }
+        
+        
+                
+    }
+
+
+
+
+    
+
+
